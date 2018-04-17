f18a55750b2415d32e9b7839050e95a8f8bba84e
gsoules <gsoules@avantlogic.com>
2018-04-17 08:10:35 -0400

    Add option to set layout selector width.

--- config_form.php
+++ config_form.php
@@ -4,7 +4,7 @@ $view = get_view();
 $searchElements = get_option('avantsearch_elements');
 if (empty(trim($searchElements)))
 {
-    $searchELements = '<identifier>: Item;' . PHP_EOL . '<title>:Title;';
+    $searchELements = 'Identifier: Item;' . PHP_EOL . 'Title;' . PHP_EOL . 'Type;' . PHP_EOL . 'Subject;';
     set_option('avantsearch_elements', $searchElements);
 }
 
@@ -12,9 +12,14 @@ $layouts = get_option('avantsearch_layouts');
 if (empty(trim($layouts)))
 {
     $layouts = 'L1, public, Details;';
+    $layouts .= 'L2, public, Type / Subject: Type, Subject;';
     set_option('avantsearch_layouts', $layouts);
 }
 ?>
+
+<div class="plugin-help">
+    <a href="https://github.com/gsoules/AvantSearch#usage" target="_blank">Learn about the configuration options on this page</a>
+</div>
 
 <hr/>
 <h4>Read the <a href="https://github.com/gsoules/AvantSearch/blob/master/README.md" target="_blank">AvantSearch documentation</a> before choosing any of these options.</h4>
@@ -89,6 +94,17 @@ if (empty(trim($layouts)))
         <?php echo $view->formTextarea('avantsearch_layouts', $layouts, array('rows'=>'8','cols'=>'40')); ?>
     </div>
 </div>
+
+<div class="field">
+    <div class="two columns alpha">
+        <label for="avantsearch_layout_selector_width"><?php echo __('Layout Selector Width'); ?></label>
+    </div>
+    <div class="inputs five columns">
+        <p class="explanation"><?php echo __('The width of the layout selector dropdown.'); ?></p>
+        <?php echo $view->formText('avantsearch_layout_selector_width', get_option('avantsearch_layout_selector_width')); ?>
+    </div>
+</div>
+
 
 <div class="field">
     <div class="two columns alpha">
