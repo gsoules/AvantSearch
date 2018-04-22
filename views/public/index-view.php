711aa9db7298b482a5a0b9145a47143c144b97f1
gsoules <gsoules@avantlogic.com>
2018-04-22 18:15:12 -0400

    Refactor and clean up the code.

--- views/public/index-view.php
+++ views/public/index-view.php
@@ -1,94 +1,9 @@
 <?php
 /* @var $searchResults SearchResultsIndexView */
 
-$results = $searchResults->getResults();
-$totalResults = count($results);
-$indexFieldElementId = $searchResults->getIndexFieldElementId();
-$showLetterIndex = $totalResults > 50;
-$element = get_db()->getTable('Element')->find($indexFieldElementId);
-$indexFieldName = empty($element) ? '' : $element['name'];
-$pageTitle = SearchResultsView::getSearchResultsMessage($totalResults);
-
-echo head(array('title' => $pageTitle));
-echo "<div class='search-results-container'>";
-echo "<div class='search-results-title'>$pageTitle</div>";
-
-echo $searchResults->emitModifySearchButton();
-echo $searchResults->emitSearchFilters(__('Index View by %s', $indexFieldName), $totalResults ? pagination_links() : '', false);
-
-if ($totalResults)
+function emitEntries($entries, $indexFieldElementId, $searchResults)
 {
-    $entries = array();
-    foreach ($results as $result)
-    {
-        // Get the text from the pseudo column text_exp emitted by the SQL query for index views.
-        $text = $result['text_exp'];
-        $count = $result['count'];
-        if (isset($entries[$text]))
-        {
-            // Another entry has the same leaf text as another entry that shares the same ancestry.
-            // Add the counts of all the duplicate entries and apply them to a single unique entry.
-            $count += $entries[$text]['count'];
-        }
-        $entries[$text]['count'] = $count;
-        $entries[$text]['id'] = $result['id'];
-    }
-
-    if ($showLetterIndex)
-    {
-        // Determine which letters to enable in the letter index.
-        $letters = array('#' => false) + array_fill_keys(range('A', 'Z'), false);
-        foreach ($entries as $entryText => $entry)
-        {
-            if (empty($entryText))
-            {
-                continue;
-            }
-
-            // Get the entry's first letter. If it's a double quote, use the second letter instead.
-            $firstLetter = substr($entryText, 0, 1);
-            if ($firstLetter == '"' && strlen($entryText) > 1)
-            {
-                $firstLetter = substr($entryText, 1, 1);
-            }
-
-            // If the first letter is not A-Z, treat is as a number so that it groups with the '#" index.
-            if (preg_match('/[^a-zA-Z]+/', $firstLetter))
-            {
-                $letters['#'] = true;
-            }
-            else
-            {
-                $letters[strtoupper($firstLetter)] = true;
-            }
-        }
-
-        // Create the letter index. Don't include '#' in the index if it's not set.
-        $letterIndex = '<ul>';
-        foreach ($letters as $letter => $isSet)
-        {
-            if ($isSet)
-            {
-                $letterId = $letter == '#' ? 'other' : $letter;
-                $letterIndex .= sprintf('<li><a href="#%s">%s</a></li>', $letterId, $letter);
-            }
-            elseif ($letter != '#')
-            {
-                $letterIndex .= sprintf('<li><span>%s</span></li>', $letter);
-            }
-        }
-        $letterIndex .= '</ul>';
-
-        // Emit the letter index at the top of the page.
-        echo '<div class="search-index-view-index" id="search-index-view-index-top">';
-        echo $letterIndex;
-        echo '</div>';
-    }
-
-    // Emit the result entries.
-    echo '<div id="search-index-view-headings">';
     $currentHeading = '';
-    $headingId = '';
     foreach ($entries as $entryText => $entry)
     {
         if (empty($entryText))
@@ -130,7 +45,8 @@ if ($totalResults)
             // Emit a link to produce search results showing all items for this entry.
             if ($indexFieldElementId != 0)
             {
-                $url = $searchResults->emitIndexEntryUrl($entryText, $indexFieldElementId, 'ends with');
+                $searchCondition = $entry['hierarchy'] ? 'ends with' : 'is exactly';
+                $url = $searchResults->emitIndexEntryUrl($entryText, $indexFieldElementId, $searchCondition);
                 echo "<a href=\"$url\">$entryText</a>";
             }
             else
@@ -145,13 +61,125 @@ if ($totalResults)
         }
         echo "</p>";
     }
+}
+
+function emitLetterIndex($entries)
+{
+    // Determine which letters to enable in the letter index.
+    $letters = array('#' => false) + array_fill_keys(range('A', 'Z'), false);
+    foreach ($entries as $entryText => $entry)
+    {
+        if (empty($entryText))
+        {
+            continue;
+        }
+
+        // Get the entry's first letter. If it's a double quote, use the second letter instead.
+        $firstLetter = substr($entryText, 0, 1);
+        if ($firstLetter == '"' && strlen($entryText) > 1)
+        {
+            $firstLetter = substr($entryText, 1, 1);
+        }
+
+        // If the first letter is not A-Z, treat is as a number so that it groups with the '#" index.
+        if (preg_match('/[^a-zA-Z]+/', $firstLetter))
+        {
+            $letters['#'] = true;
+        }
+        else
+        {
+            $letters[strtoupper($firstLetter)] = true;
+        }
+    }
+
+    // Create the letter index. Don't include '#' in the index if it's not set.
+    $letterIndex = '<ul>';
+    foreach ($letters as $letter => $isSet)
+    {
+        if ($isSet)
+        {
+            $letterId = $letter == '#' ? 'other' : $letter;
+            $letterIndex .= sprintf('<li><a href="#%s">%s</a></li>', $letterId, $letter);
+        }
+        elseif ($letter != '#')
+        {
+            $letterIndex .= sprintf('<li><span>%s</span></li>', $letter);
+        }
+    }
+    $letterIndex .= '</ul>';
+
+    // Emit the letter index at the top of the page.
+    echo '<div class="search-index-view-index" id="search-index-view-index-top">';
+    echo $letterIndex;
+    echo '</div>';
+    return $letterIndex;
+}
+
+function flattenResults($results, $indexFieldElementId)
+{
+    // Combine results into unique entries. This is necessary because some results can have the same
+    // leaf value, but a different ancestry. This can be due to a data entry error, or an obscure case
+    // e.g. 'Schoodic, Acadia National Park' and "MDI, Acadia National Park'. Create a unique entry
+    // with a count representing the total of all the results with the same leaf text.
+
+    $hierarchyElements = SearchOptions::getOptionDataForHierarchy();
+    $entries = array();
+    foreach ($results as $result)
+    {
+        $isHierarchyElement = array_key_exists($indexFieldElementId, $hierarchyElements);
+
+        // For hierarchy elements, get the text from the pseudo column text_exp emitted by the SQL query for index views.
+        $text = $isHierarchyElement ? $result['text_exp'] : $result['text'];
+
+        $count = $result['count'];
+
+        if (isset($entries[$text]))
+        {
+            $count += $entries[$text]['count'];
+        }
+        $entries[$text]['count'] = $count;
+        $entries[$text]['id'] = $result['id'];
+        $entries[$text]['hierarchy'] = $isHierarchyElement;
+    }
+    return $entries;
+}
+
+$results = $searchResults->getResults();
+$totalResults = count($results);
+$indexFieldElementId = $searchResults->getIndexFieldElementId();
+$showLetterIndex = $totalResults > 50;
+$element = get_db()->getTable('Element')->find($indexFieldElementId);
+$indexFieldName = empty($element) ? '' : $element['name'];
+$pageTitle = SearchResultsView::getSearchResultsMessage($totalResults);
+
+
+echo head(array('title' => $pageTitle));
+echo "<div class='search-results-container'>";
+echo "<div class='search-results-title'>$pageTitle</div>";
+
+echo $searchResults->emitModifySearchButton();
+echo $searchResults->emitSearchFilters(__('Index View by %s', $indexFieldName), $totalResults ? pagination_links() : '', false);
+
+if ($totalResults)
+{
+    $entries = flattenResults($results, $indexFieldElementId);
+
+    if ($showLetterIndex)
+    {
+        $letterIndex = emitLetterIndex($entries);
+    }
+
+    echo '<div id="search-index-view-headings">';
+    emitEntries($entries, $indexFieldElementId, $searchResults);
     echo "</div>";
+
     if ($showLetterIndex)
     {
         echo '<div class="search-index-view-index" id="search-index-view-index-bottom">';
         echo $letterIndex;
         echo '</div>';
     }
+
     echo '</div>';
 }
 else
