0cf66ca3d8a5ff6f08dc31bb75e3f0154bcc8b68
gsoules <gsoules@avantlogic.com>
2019-04-03 17:38:42 -0400

    Create ElasticsearchIndexBuilder.php

---
+++ models/ElasticsearchIndexBuilder.php
@@ -0,0 +1,44 @@
+<?php
+
+class ElasticsearchIndexBuilder extends AvantElasticsearch
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function fetchObjects($className)
+    {
+        if (!class_exists($className))
+        {
+            return null;
+        }
+        $db = get_db();
+        $table = $db->getTable($className);
+        $select = $table->getSelect();
+        $table->applySorting($select, 'id', 'ASC');
+        return $table->fetchObjects($select);
+    }
+
+    public function indexAll()
+    {
+        $items = $this->fetchObjects('Item');
+        if (empty($items))
+        {
+            return;
+        }
+
+        $responses = $this->performBulkIndex($items);
+
+        foreach ($responses as $response)
+        {
+            if (isset($response['error']))
+            {
+                $error = $response['error'];
+                $msg = $response['_id'] . ' : ' . $error['type'] . ' - ' . $error['reason'] . ' - ' . $error['caused_by']['reason'];
+                $flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+                $flash->addMessage($msg);
+            }
+        }
+    }
+}
