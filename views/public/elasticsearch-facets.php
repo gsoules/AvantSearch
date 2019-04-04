585778234f08a17273441f007554d80c1c81d10f
gsoules <gsoules@avantlogic.com>
2019-04-04 14:44:15 -0400

    Code cleanup.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -1,20 +1,22 @@
 <?php
 $avantElasticsearchFacets = new AvantElasticsearchFacets();
-$aggregation_labels = $avantElasticsearchFacets->getFacetNames();
-$querystr = Elasticsearch_Utils::getQueryString($query);
-$applied_facets = $query['facets'];
+$facetLabels = $avantElasticsearchFacets->getFacetNames();
+$queryString = $avantElasticsearchFacets->createQueryStringWithFacets($query);
+$appliedFacets = $query['facets'];
 ?>
 
-<?php if(count($applied_facets) > 0): ?>
+<?php if(count($appliedFacets) > 0): ?>
 <div id="elasticsearch-filters-active">
     <div class="elasticsearch-facet-section">Applied Filters <a style="font-size: 80%" href="<?php echo get_view()->url('/find').'?query='.urlencode($query['query']); ?>">[Reset]</a></div>
 <ul>
-    <?php foreach($applied_facets as $facet_name => $facet_values): ?>
-        <?php $facet_label = htmlspecialchars($aggregation_labels[$facet_name]); ?>
-        <?php $facet_value = htmlspecialchars(Elasticsearch_Utils::facetVal2Str($facet_values)); ?>
+    <?php foreach($appliedFacets as $facetName => $facetValues): ?>
+        <?php
+        $facetLabel = htmlspecialchars($facetLabels[$facetName]);
+        $facetValue = htmlspecialchars($avantElasticsearchFacets->convertFacetValuesToString($facetValues));
+        ?>
         <li>
-            <?php echo "$facet_label</br><i>$facet_value</i>"; ?>
-            <a href="<?php echo get_view()->url('/find') . '?' . Elasticsearch_Utils::removeFacetFromQuery($querystr, $facet_name); ?>">[&#10006;]</a>
+            <?php echo "$facetLabel</br><i>$facetValue</i>"; ?>
+            <a href="<?php echo get_view()->url('/find') . '?' . $avantElasticsearchFacets->removeFacetFromQuery($queryString, $facetName); ?>">[&#10006;]</a>
         </li>
     <?php endforeach ?>
 
@@ -24,16 +26,18 @@ $applied_facets = $query['facets'];
 
 <div id="elasticsearch-filters">
 <div class="elasticsearch-facet-section">Filters</div>
-<?php foreach($aggregation_labels as $agg_name => $agg_label): ?>
-    <?php if(count($aggregations[$agg_name]['buckets']) > 0): ?>
-    <div class="elasticsearch-facet-name"><?php echo $agg_label; ?></div>
+<?php foreach($facetLabels as $aggregateName => $aggregateLabel): ?>
+    <?php if(count($aggregations[$aggregateName]['buckets']) > 0): ?>
+    <div class="elasticsearch-facet-name"><?php echo $aggregateLabel; ?></div>
         <ul>
-            <?php $buckets = $aggregations[$agg_name]['buckets']; ?>
-            <?php foreach($aggregations[$agg_name]['buckets'] as $agg): ?>
-                <?php $facet_url = get_view()->url('/find') . '?' . Elasticsearch_Utils::addFacetToQuery($querystr, $agg_name, $agg['key']); ?>
-                <?php $agg_key = isset($agg['key_as_string']) ? $agg['key_as_string'] : $agg['key']; ?>
-                <?php $agg_count = $agg['doc_count']; ?>
-                <li><a href="<?php echo $facet_url; ?>"><?php echo htmlspecialchars(__($agg_key)); ?></a> <?php echo " (".$agg['doc_count'].")"; ?></li>
+            <?php $buckets = $aggregations[$aggregateName]['buckets']; ?>
+            <?php foreach ($aggregations[$aggregateName]['buckets'] as $aggregate): ?>
+                <?php
+                $facetUrl = get_view()->url('/find') . '?' . $avantElasticsearchFacets->addFacetToQuery($queryString, $aggregateName, $aggregate['key']);
+                $aggregateKey = isset($aggregate['key_as_string']) ? $aggregate['key_as_string'] : $aggregate['key'];
+                $aggregateCount = $aggregate['doc_count'];
+                ?>
+                <li><a href="<?php echo $facetUrl; ?>"><?php echo htmlspecialchars(__($aggregateKey)); ?></a> <?php echo " (".$aggregate['doc_count'].")"; ?></li>
             <?php endforeach; ?>
         </ul>
     <?php endif; ?>
