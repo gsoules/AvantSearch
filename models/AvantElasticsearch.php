e47fe36e6b5620f9607d7463ae7889f281eff2f9
gsoules <gsoules@avantlogic.com>
2019-04-05 13:50:52 -0400

    Add new methods.

--- models/AvantElasticsearch.php
+++ models/AvantElasticsearch.php
@@ -6,8 +6,7 @@ class AvantElasticsearch
     public function __construct()
     {
         $this->docIndex = $this->getElasticsearchIndexName();
-        if (empty($this->docIndex))
-        {
+        if (empty($this->docIndex)) {
             $this->docIndex = Elasticsearch_Config::index();
         }
     }
@@ -24,8 +23,32 @@ class AvantElasticsearch
         return Elasticsearch_Client::create($options);
     }
 
+    public function getDocumentMappingType()
+    {
+        return '_doc';
+    }
+
     public function getElasticsearchIndexName()
     {
         return get_option('elasticsearch_index');
+    }
+
+    public function getDocumentIdForItem($item)
+    {
+        // Create an id that is unique among all organizations that have items in the index.
+        // The item Id alone is not sufficient since multiple organizations may have an item with
+        // that Id. However, the item Id combined with the item's owner Id is unique. The owner
+        // Id is the item type e.g. SWHPL or GCIHS since these are unique to each organization.
+
+        $ownerId = self::getOwnerIdForItem($item);
+        $documentId = "$ownerId-$item->id";
+        return $documentId;
+    }
+
+    public function getOwnerIdForItem($item)
+    {
+        $itemType = $item->getItemType();
+        $ownerId = strtolower($itemType->name);
+        return $ownerId;
     }
 }
