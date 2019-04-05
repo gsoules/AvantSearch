dde73da927a1a1c3d0f9531dee7aaf9267554207
gsoules <gsoules@avantlogic.com>
2019-04-05 10:04:42 -0400

    Improve comment.

--- models/AvantElasticsearch.php
+++ models/AvantElasticsearch.php
@@ -14,7 +14,8 @@ class AvantElasticsearch
 
     public function convertElementNameToElasticsearchFieldName($elementName)
     {
-        // Convert the element name to lowercase and strip away spaces and other non-alphanumberic characters.
+        // Convert the element name to lowercase and strip away spaces and other non-alphanumberic characters
+        // as required by Elasticsearch syntax.
         return strtolower(preg_replace('/[^a-zA-Z0-9-_]/', '', $elementName));
     }
 
