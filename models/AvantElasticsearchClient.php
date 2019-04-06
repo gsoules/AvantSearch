651dfb783b7affaec21c62d3f22ff8527808e589
gsoules <gsoules@avantlogic.com>
2019-04-06 16:06:43 -0400

    Get configuration values from ini file.

--- models/AvantElasticsearchClient.php
+++ models/AvantElasticsearchClient.php
@@ -15,18 +15,21 @@ class AvantElasticsearchClient extends AvantElasticsearch
 
     public static function create(array $options = array())
     {
+        $configFileName = AVANTSEARCH_PLUGIN_DIRECTORY . DIRECTORY_SEPARATOR . 'config.ini';
+        $config = new Zend_Config_Ini($configFileName, 'elasticsearch');
+
         $timeout = isset($options['timeout']) ? $options['timeout'] : 90;
         $nobody = isset($options['nobody']) ? $options['nobody'] : false;
 
         $builder = ClientBuilder::create();
 
-        $hosts = self::getHosts();
+        $hosts = self::getHosts($config);
         if (isset($hosts))
         {
             $builder->setHosts($hosts);
         }
 
-        $handler = self::getHandler();
+        $handler = self::getHandler($config);
         if (isset($handler))
         {
             $builder->setHandler($handler);
@@ -43,30 +46,27 @@ class AvantElasticsearchClient extends AvantElasticsearch
         return $client;
     }
 
-    protected static function getHandler()
+    protected static function getHandler($config)
     {
         // Provide a signing handler for use with the official Elasticsearch-PHP client.
         // The handler will load AWS credentials and send requests using a RingPHP cURL handler.
         // Without this handler, a curl request to Elasticsearch on AWS will return a 403 Forbidden response.
 
-        $key = 'AKIAQ2V2PCHKL5FIQNUD';
-        $secret = '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';
+        $key = $config->key;
+        $secret = $config->secret;
         $creds = new Credentials($key, $secret);
-        $region = 'us-east-2';
+        $region = $config->region;
         $provider = CredentialProvider::fromCredentials($creds);
 
         return new ElasticsearchPhpHandler($region, $provider);
     }
 
-    protected static function getHosts()
+    protected static function getHosts($config)
     {
-        $hostPath = 'search-digitalarchive-6wn5q4bmsxnikvykh7xiswwo4q.us-east-2.es.amazonaws.com';
-        $port = '443';
-
         $host = [
-            'host' => $hostPath,
-            'port' => $port,
-            'scheme' => "https",
+            'host' => $config->host,
+            'port' => $config->port,
+            'scheme' => $config->scheme,
             'user' => '',
             'pass' => ''
         ];
