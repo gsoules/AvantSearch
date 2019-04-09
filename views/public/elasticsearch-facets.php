7cd8f0fd11c534842c740f72db4db390691fab39
gsoules <gsoules@avantlogic.com>
2019-04-09 09:50:59 -0400

    Further rework on logic to emit facet HTML.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -3,6 +3,7 @@ $avantElasticsearchFacets = new AvantElasticsearchFacets();
 $facetLabels = $avantElasticsearchFacets->getFacetNames();
 $queryString = $avantElasticsearchFacets->createQueryStringWithFacets($query);
 $appliedFacets = $query['facet'];
+$findUrl = get_view()->url('/find');
 ?>
 
 <?php if (count($appliedFacets) > 0): ?>
@@ -12,7 +13,7 @@ $appliedFacets = $query['facet'];
                href="<?php echo get_view()->url('/find') . '?query=' . urlencode($query['query']); ?>">[Reset]</a>
         </div>
         <?php
-        $filters = '';
+        $appliedFilters = '';
         foreach ($appliedFacets as $facetName => $facetValues)
         {
             $facetLabel = htmlspecialchars($facetLabels[$facetName]);
@@ -21,38 +22,41 @@ $appliedFacets = $query['facet'];
                 $facetValues = array($facetValues);
             }
             $facetValue = htmlspecialchars($avantElasticsearchFacets->convertFacetValuesToString($facetValues));
-            $findUrl = get_view()->url('/find');
             $resetLink = $avantElasticsearchFacets->removeFacetFromQuery($queryString, $facetName);
-            $filters .= '<li>';
-            $filters .=  "$facetLabel</br><i>$facetValue</i>";
-            $filters .= '<a href="' . $findUrl . '?' . $resetLink . '"> [&#10006;]</a>';
-            $filters .= '</li>';
+            $appliedFilters .= '<li>';
+            $appliedFilters .=  "$facetLabel</br><i>$facetValue</i>";
+            $appliedFilters .= '<a href="' . $findUrl . '?' . $resetLink . '"> [&#10006;]</a>';
+            $appliedFilters .= '</li>';
         }
-        $list = "<ul>$filters</ul>";
-        echo $list;
+        echo "<ul>$appliedFilters</ul>";
         ?>
     </div>
 <?php endif; ?>
 
 <div id="elasticsearch-filters">
     <div class="elasticsearch-facet-section">Filters</div>
-    <?php foreach ($facetLabels as $aggregateName => $aggregateLabel): ?>
-        <?php if (count($aggregations[$aggregateName]['buckets']) > 0): ?>
-            <div class="elasticsearch-facet-name"><?php echo $aggregateLabel; ?></div>
-            <ul>
-                <?php $buckets = $aggregations[$aggregateName]['buckets']; ?>
-                <?php foreach ($aggregations[$aggregateName]['buckets'] as $aggregate): ?>
-                    <?php
-                    $facetUrl = get_view()->url('/find') . '?' . $avantElasticsearchFacets->createFacetLink($queryString, $aggregateName, $aggregate['key']);
-                    $aggregateKey = isset($aggregate['key_as_string']) ? $aggregate['key_as_string'] : $aggregate['key'];
-                    $aggregateCount = $aggregate['doc_count'];
-                    ?>
-                    <li>
-                        <a href="<?php echo $facetUrl; ?>"><?php echo htmlspecialchars(__($aggregateKey)); ?></a> <?php echo " (" . $aggregate['doc_count'] . ")"; ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-    <?php endforeach; ?>
+    <?php
+    foreach ($facetLabels as $aggregateName => $aggregateLabel)
+    {
+        if (count($aggregations[$aggregateName]['buckets']) == 0)
+        {
+            continue;
+        }
+        echo '<div class="elasticsearch-facet-name">' . $aggregateLabel . '</div>';
+        $filters = '';
+        $buckets = $aggregations[$aggregateName]['buckets'];
+        foreach ($aggregations[$aggregateName]['buckets'] as $aggregate)
+        {
+            $filterLink = $avantElasticsearchFacets->createFacetLink($queryString, $aggregateName, $aggregate['key']);
+            $facetUrl = $findUrl . '?' . $filterLink;
+            $aggregateKey = $aggregate['key'];
+            $aggregateCount = $aggregate['doc_count'];
+            $filters .= '<li>';
+            $filters .= '<a href="' . $facetUrl . '">' . htmlspecialchars(__($aggregateKey)) . '</a> (' . $aggregate['doc_count'] . ')';
+            $filters .= '</li>';
+        }
+        echo "<ul>$filters</ul>";
+    }
+    ?>
 </div>
 
