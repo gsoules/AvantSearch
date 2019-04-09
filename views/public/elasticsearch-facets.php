184b6f787f16146154253283f8533c44883db578
gsoules <gsoules@avantlogic.com>
2019-04-09 08:27:20 -0400

    Initial rework of logic to emit facet HTML.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -5,41 +5,54 @@ $queryString = $avantElasticsearchFacets->createQueryStringWithFacets($query);
 $appliedFacets = $query['facet'];
 ?>
 
-<?php if(count($appliedFacets) > 0): ?>
-<div id="elasticsearch-filters-active">
-    <div class="elasticsearch-facet-section">Applied Filters <a style="font-size: 80%" href="<?php echo get_view()->url('/find').'?query='.urlencode($query['query']); ?>">[Reset]</a></div>
-<ul>
-    <?php foreach($appliedFacets as $facetName => $facetValues): ?>
+<?php if (count($appliedFacets) > 0): ?>
+    <div id="elasticsearch-filters-active">
+        <div class="elasticsearch-facet-section">Applied Filters
+            <a class="elasticsearch-facet-reset"
+               href="<?php echo get_view()->url('/find') . '?query=' . urlencode($query['query']); ?>">[Reset]</a>
+        </div>
         <?php
-        $facetLabel = htmlspecialchars($facetLabels[$facetName]);
-        $facetValue = htmlspecialchars($avantElasticsearchFacets->convertFacetValuesToString($facetValues));
+        $filters = '';
+        foreach ($appliedFacets as $facetName => $facetValues)
+        {
+            $facetLabel = htmlspecialchars($facetLabels[$facetName]);
+            if (!is_array($facetValues))
+            {
+                $facetValues = array($facetValues);
+            }
+            $facetValue = htmlspecialchars($avantElasticsearchFacets->convertFacetValuesToString($facetValues));
+            $findUrl = get_view()->url('/find');
+            $resetLink = $avantElasticsearchFacets->removeFacetFromQuery($queryString, $facetName);
+            $filters .= '<li>';
+            $filters .=  "$facetLabel</br><i>$facetValue</i>";
+            $filters .= '<a href="' . $findUrl . '?' . $resetLink . '"> [&#10006;]</a>';
+            $filters .= '</li>';
+        }
+        $list = "<ul>$filters</ul>";
+        echo $list;
         ?>
-        <li>
-            <?php echo "$facetLabel</br><i>$facetValue</i>"; ?>
-            <a href="<?php echo get_view()->url('/find') . '?' . $avantElasticsearchFacets->removeFacetFromQuery($queryString, $facetName); ?>">[&#10006;]</a>
-        </li>
-    <?php endforeach ?>
-
-</ul>
-</div>
+    </div>
 <?php endif; ?>
 
 <div id="elasticsearch-filters">
-<div class="elasticsearch-facet-section">Filters</div>
-<?php foreach($facetLabels as $aggregateName => $aggregateLabel): ?>
-    <?php if(count($aggregations[$aggregateName]['buckets']) > 0): ?>
-    <div class="elasticsearch-facet-name"><?php echo $aggregateLabel; ?></div>
-        <ul>
-            <?php $buckets = $aggregations[$aggregateName]['buckets']; ?>
-            <?php foreach ($aggregations[$aggregateName]['buckets'] as $aggregate): ?>
-                <?php
-                $facetUrl = get_view()->url('/find') . '?' . $avantElasticsearchFacets->createFacetLink($queryString, $aggregateName, $aggregate['key']);
-                $aggregateKey = isset($aggregate['key_as_string']) ? $aggregate['key_as_string'] : $aggregate['key'];
-                $aggregateCount = $aggregate['doc_count'];
-                ?>
-                <li><a href="<?php echo $facetUrl; ?>"><?php echo htmlspecialchars(__($aggregateKey)); ?></a> <?php echo " (".$aggregate['doc_count'].")"; ?></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-<?php endforeach; ?>
+    <div class="elasticsearch-facet-section">Filters</div>
+    <?php foreach ($facetLabels as $aggregateName => $aggregateLabel): ?>
+        <?php if (count($aggregations[$aggregateName]['buckets']) > 0): ?>
+            <div class="elasticsearch-facet-name"><?php echo $aggregateLabel; ?></div>
+            <ul>
+                <?php $buckets = $aggregations[$aggregateName]['buckets']; ?>
+                <?php foreach ($aggregations[$aggregateName]['buckets'] as $aggregate): ?>
+                    <?php
+                    $facetUrl = get_view()->url('/find') . '?' . $avantElasticsearchFacets->createFacetLink($queryString, $aggregateName, $aggregate['key']);
+                    $aggregateKey = isset($aggregate['key_as_string']) ? $aggregate['key_as_string'] : $aggregate['key'];
+                    $aggregateCount = $aggregate['doc_count'];
+                    ?>
+                    <li>
+                        <a href="<?php echo $facetUrl; ?>"><?php echo htmlspecialchars(__($aggregateKey)); ?></a> <?php echo " (" . $aggregate['doc_count'] . ")"; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    <?php endforeach; ?>
 </div>
+
