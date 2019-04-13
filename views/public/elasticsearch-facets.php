bb79d67b8f34fd670408239458bd73a004f6dc89
gsoules <gsoules@avantlogic.com>
2019-04-13 12:16:25 -0400

    Only show leaf facet values when at least one facet applied.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -3,11 +3,12 @@ $avantElasticsearchFacets = new AvantElasticsearchFacets();
 $facetNames = $avantElasticsearchFacets->getFacetNames();
 $queryString = $avantElasticsearchFacets->createQueryStringWithFacets($query);
 $appliedFacets = $query['facet'];
+$facetsAreApplied = count($appliedFacets) > 0;
 $findUrl = get_view()->url('/find');
 $appliedFacetValues = array();
 ?>
 
-<?php if (count($appliedFacets) > 0): ?>
+<?php if ($facetsAreApplied): ?>
     <div id="elasticsearch-filters-active">
         <div class="elasticsearch-facet-section">Applied Filters
             <a class="elasticsearch-facet-reset"
@@ -42,16 +43,15 @@ $appliedFacetValues = array();
 <div id="elasticsearch-filters">
     <div class="elasticsearch-facet-section">Filters</div>
     <?php
+
     foreach ($facetNames as $facetName => $facetLabel)
     {
-        if ($facetName == 'tag')
-        {
-            // The tag facet is fully supported, but for now simply don't show it.
-            continue;
-        }
+        $buckets = $aggregations[$facetName]['buckets'];
 
-        if (count($aggregations[$facetName]['buckets']) == 0)
+        if (count($buckets) == 0 || $facetName == 'tag')
         {
+            // Don't display empty buckets or tags. The tag facet is supported, but for now don't show it.
+            // TO-DO: Make display of the tag facet a configuration option.
             continue;
         }
 
@@ -60,9 +60,21 @@ $appliedFacetValues = array();
         $filters = '';
         $buckets = $aggregations[$facetName]['buckets'];
 
-        foreach ($aggregations[$facetName]['buckets'] as $bucket)
+        foreach ($buckets as $bucket)
         {
             $bucketValue = $bucket['key'];
+
+            $isLeaf = strpos($bucketValue, ',') !== false;
+
+            if (!$facetsAreApplied)
+            {
+                if ($isLeaf)
+                {
+                    // Don't show leafs until at least one facet is applied.
+                    continue;
+                }
+            }
+
             $applied = in_array($bucketValue, $appliedFacetValues);
             $text = htmlspecialchars($bucketValue);
             $count = ' (' . $bucket['doc_count'] . ')';
@@ -70,7 +82,7 @@ $appliedFacetValues = array();
             if ($applied)
             {
                 // Don't provide a link for a facet that's already been applied.
-                $filter = $text . $count;
+                $filter = $text;
             }
             else
             {
@@ -80,7 +92,8 @@ $appliedFacetValues = array();
                 $filter = '<a href="' . $facetUrl . '">' . $text . '</a>' . $count;
             }
 
-            $filters .= "<li>$filter</li>";
+            $class = $isLeaf ? " class='elasticsearch-facet-leaf'" : '';
+            $filters .= "<li$class>$filter</li>";
         }
 
         echo "<ul>$filters</ul>";
