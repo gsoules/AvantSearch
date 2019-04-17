030463c2c7e8830458438c0760a89544c5255f85
gsoules <gsoules@avantlogic.com>
2019-04-17 15:58:53 -0400

    Improved logic to display level 1 and level 2 facets.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -4,6 +4,7 @@ $facetDefinitions = $avantElasticsearchFacets->getFacetDefinitions();
 $queryString = $avantElasticsearchFacets->createQueryStringWithFacets($query);
 
 $appliedFacets = $query['facet'];
+$appliedFacetIds = array();
 $facetsAreApplied = count($appliedFacets) > 0;
 $appliedFacetValues = array();
 
@@ -26,6 +27,8 @@ $findUrl = get_view()->url('/find');
                 // This should only happen if the query string syntax in invalid because someone edited or mistyped it.
                 break;
             }
+
+            $appliedFacetIds[$facetId] = true;
 
             $facetName = htmlspecialchars($facetDefinitions[$facetId]['name']);
             $appliedFilters .= '<div class="elasticsearch-facet-name">' . $facetName . '</div>';
@@ -70,17 +73,44 @@ $findUrl = get_view()->url('/find');
         foreach ($buckets as $bucket)
         {
             $bucketValue = $bucket['key'];
+            $text = htmlspecialchars($bucketValue);
+            $class = '';
 
-            $isLeaf = strpos($bucketValue, ',') !== false;
-
-            if (!$facetsAreApplied && $isLeaf)
+            if ($facetDefinition['is_hierarchy'])
             {
-                // Don't show leafs until at least one facet is applied.
-                continue;
+                $isRoot = false;
+
+                // Root values begin with an underscore.
+                if (strpos($bucketValue, '_') === 0)
+                {
+                    $isRoot = true;
+
+                    // Remove the underscore from the beginning of the root value text.
+                    //$text = substr($text, 1);
+                }
+
+                if (isset($appliedFacetIds[$facetId]))
+                {
+                    // This facet has been applied. Show it's leaf values indented.
+                    if ($facetDefinition['show_root'])
+                    {
+                        // Add some styling when leafs appear under roots.
+                        $level = $isRoot ? 'root' : 'leaf';
+                        $class = " class='elasticsearch-facet-$level'";
+                    }
+                }
+                else
+                {
+                    if ($facetDefinition['show_root'] && !$isRoot)
+                    {
+                        // Don't show leafs until at least one facet is applied.
+                        continue;
+                    }
+                }
             }
 
             $applied = in_array($bucketValue, $appliedFacetValues);
-            $text = htmlspecialchars($bucketValue);
+
             $count = ' (' . $bucket['doc_count'] . ')';
 
             if ($applied)
@@ -94,14 +124,6 @@ $findUrl = get_view()->url('/find');
                 $filterLink = $avantElasticsearchFacets->createAddFacetLink($queryString, $facetId, $bucketValue);
                 $facetUrl = $findUrl . '?' . $filterLink;
                 $filter = '<a href="' . $facetUrl . '">' . $text . '</a>' . $count;
-            }
-
-            $class = '';
-            if ($facetsAreApplied && $facetDefinition['show_root'])
-            {
-                // Add some styling when leafs appear under roots.
-                $level = $isLeaf ? 'leaf' : 'root';
-                $class = " class='elasticsearch-facet-$level'";
             }
 
             $filters .= "<li$class>$filter</li>";
