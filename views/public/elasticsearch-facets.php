4f85078592ab4abbfc1ae390186a69542ea4943e
gsoules <gsoules@avantlogic.com>
2019-04-20 17:07:23 -0400

    Code cleanup.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -1,18 +1,7 @@
 <?php
 $avantElasticsearchFacets = new AvantElasticsearchFacets();
 $findUrl = get_view()->url('/find');
-$queryHasFacets = isset($query[FACET_KIND_ROOT]) || isset($query[FACET_KIND_LEAF]);
 ?>
-
-<?php if ($queryHasFacets): ?>
-    <div id="elasticsearch-filters-active">
-        <div class="elasticsearch-facet-section">Applied Filters
-            <a class="elasticsearch-facet-reset"
-               href="<?php echo get_view()->url('/find') . '?query=' . urlencode($query['query']); ?>">[Reset]</a>
-        </div>
-    </div>
-<?php endif; ?>
-
 <div id="elasticsearch-filters">
     <div class="elasticsearch-facet-section">Refine your search</div>
     <?php
