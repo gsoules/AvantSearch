26c4b6b7f5f5510fdbae863d2afd0c1cf3e49366
gsoules <gsoules@avantlogic.com>
2019-04-21 06:54:38 -0400

    Method name change.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -5,7 +5,7 @@ $findUrl = get_view()->url('/find');
 <div id="elasticsearch-filters">
     <div class="elasticsearch-facet-section">Refine your search</div>
     <?php
-        echo $avantElasticsearchFacets->emitHtmlForFilters($aggregations, $query, $findUrl);
+        echo $avantElasticsearchFacets->emitHtmlForFacets($aggregations, $query, $findUrl);
     ?>
 </div>
 
