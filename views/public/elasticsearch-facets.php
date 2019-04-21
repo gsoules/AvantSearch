787e068b7dd5eed66b770071ce161c3b5b9cb154
gsoules <gsoules@avantlogic.com>
2019-04-21 08:22:44 -0400

    Code cleanup.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -3,9 +3,9 @@ $avantElasticsearchFacets = new AvantElasticsearchFacets();
 $findUrl = get_view()->url('/find');
 ?>
 <div id="elasticsearch-filters">
-    <div class="elasticsearch-facet-section">Refine your search</div>
+    <div class="facet-sections">Refine your search</div>
     <?php
-        echo $avantElasticsearchFacets->emitHtmlForFacets($aggregations, $query, $findUrl);
+        echo $avantElasticsearchFacets->emitHtmlForFacetsSidebar($aggregations, $query, $findUrl);
     ?>
 </div>
 
