facdc3e115709c04b80318f0530c68ae52223e41
gsoules <gsoules@avantlogic.com>
2019-04-24 07:48:55 -0400

    Code cleanup.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -1,11 +1,6 @@
 <?php
 $avantElasticsearchFacets = new AvantElasticsearchFacets();
 $findUrl = get_view()->url('/find');
-?>
-<div id="facet-sidebar">
-    <div class="facet-sections-title">Refine your search</div>
-    <?php
-        echo $avantElasticsearchFacets->emitHtmlForFacetsSidebar($aggregations, $query, $totalResults, $findUrl);
-    ?>
-</div>
-
+echo '<div id="facet-sidebar">';
+echo $avantElasticsearchFacets->emitHtmlForFacetsSidebar($aggregations, $query, $totalResults, $findUrl);
+echo '</div>';
