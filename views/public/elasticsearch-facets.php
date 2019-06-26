30ca79a88f077bce69c69707720910ce30d5401c
gsoules <gsoules@avantlogic.com>
2019-06-26 13:40:39 -0400

    Test for empty aggregations array.

--- views/public/elasticsearch-facets.php
+++ views/public/elasticsearch-facets.php
@@ -2,5 +2,7 @@
 $avantElasticsearchFacets = new AvantElasticsearchFacets();
 $findUrl = get_view()->url('/find');
 echo '<div id="facet-sidebar">';
+if (empty($aggregations))
+    $aggregations = array();
 echo $avantElasticsearchFacets->emitHtmlForFacetsSidebar($aggregations, $query, $totalResults, $findUrl);
 echo '</div>';
