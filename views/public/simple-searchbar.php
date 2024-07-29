f504336dfeca55a6531eb3ad829f281d51bc7f4b
Christos Sidiropoulos <csidirop@runbox.com>
2024-07-29 15:34:27 +0000

    Escape all querry requests with `htmlspecialchars()` to prevent XSS attacks.
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>
    (cherry picked from commit 82a2d23f36e201b1265a8343078e863ac8cf2b32)

--- views/public/simple-searchbar.php
+++ views/public/simple-searchbar.php
@@ -1,11 +1,11 @@
 <!-- Simple searchbar  -->
 
-<search id=simple-search>
+<search id="simple-search">
     <form id="search-form" class="advanced-search-form" action="find" aria-label="Search">
-        <input id="query" class="advanced-search-form input" name="keywords" type="search" placeholder="Search Items" value="<?php echo $_REQUEST["keywords"] ?>">
-        <input type="hidden" name="tags" value="<?php echo $_REQUEST["tags"] ?>">
-        <input type="hidden" name="year_start" value="<?php echo $_REQUEST["year_start"] ?>">
-        <input type="hidden" name="year_end" value="<?php echo $_REQUEST["year_end"] ?>">
+        <input id="query" class="advanced-search-form input" name="keywords" type="search" placeholder="Search Items" value="<?php echo htmlspecialchars($_REQUEST["keywords"], ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="hidden" name="tags" value="<?php echo htmlspecialchars($_REQUEST["tags"], ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="hidden" name="year_start" value="<?php echo htmlspecialchars($_REQUEST["year_start"], ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="hidden" name="year_end" value="<?php echo htmlspecialchars($_REQUEST["year_end"], ENT_QUOTES, 'UTF-8'); ?>">
         <button id="submit_search" class="advanced-search-form button" type="submit"></button>
         <a href="find/advanced" id="advanced-search-link">→ Advanced Search</a>
     </form>
