28e98b35de0d36730a6bff38c0ed4a8a5901cc95
Christos Sidiropoulos <dev@csidirop.de>
2024-10-25 14:03:09 +0000

    Use `$_GET` instead of `$_REQUEST`
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>
    (cherry picked from commit ff4e37acc590c1f8ebaa2e6d4b76486f7781bfa9)

--- views/public/simple-searchbar.php
+++ views/public/simple-searchbar.php
@@ -2,11 +2,11 @@
 
 <search id="simple-search">
     <form id="search-form" class="advanced-search-form" action="find" aria-label="Search">
-        <input id="query" class="advanced-search-form input" name="keywords" type="search" placeholder="Search Items" value="<?php echo htmlspecialchars($_REQUEST["keywords"] ?? "", ENT_QUOTES, 'UTF-8'); ?>">
-        <?php if (isset($_REQUEST["condition"])) : ?> <input type="hidden" name="condition" value="<?php echo htmlspecialchars($_REQUEST["condition"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
-        <?php if (isset($_REQUEST["tags"])) : ?> <input type="hidden" name="tags" value="<?php echo htmlspecialchars($_REQUEST["tags"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
-        <?php if (isset($_REQUEST["year_start"])) : ?> <input type="hidden" name="year_start" value="<?php echo htmlspecialchars($_REQUEST["year_start"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
-        <?php if (isset($_REQUEST["year_end"])) : ?> <input type="hidden" name="year_end" value="<?php echo htmlspecialchars($_REQUEST["year_end"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
+        <input id="query" class="advanced-search-form input" name="keywords" type="search" placeholder="Search Items" value="<?php echo htmlspecialchars($_GET["keywords"] ?? "", ENT_QUOTES, 'UTF-8'); ?>">
+        <?php if (isset($_GET["condition"])) : ?>  <input type="hidden" name="condition" value="<?php echo htmlspecialchars($_GET["condition"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
+        <?php if (isset($_GET["tags"])) : ?>       <input type="hidden" name="tags" value="<?php echo htmlspecialchars($_GET["tags"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
+        <?php if (isset($_GET["year_start"])) : ?> <input type="hidden" name="year_start" value="<?php echo htmlspecialchars($_GET["year_start"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
+        <?php if (isset($_GET["year_end"])) : ?>   <input type="hidden" name="year_end" value="<?php echo htmlspecialchars($_GET["year_end"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
         <button id="submit_search" class="advanced-search-form button" type="submit"></button>
         <a href="find/advanced" id="advanced-search-link">→ Advanced Search</a>
     </form>
