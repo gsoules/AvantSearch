a283166dc1dbb489a86dffe65267b35b76ff5a65
Christos Sidiropoulos <dev@csidirop.de>
2024-10-25 13:40:34 +0000

    Use only set queries
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>
    (cherry picked from commit 1fc963e350518e3e89007eb7cec63c911334182f)

--- views/public/simple-searchbar.php
+++ views/public/simple-searchbar.php
@@ -2,10 +2,10 @@
 
 <search id="simple-search">
     <form id="search-form" class="advanced-search-form" action="find" aria-label="Search">
-        <input id="query" class="advanced-search-form input" name="keywords" type="search" placeholder="Search Items" value="<?php echo htmlspecialchars($_REQUEST["keywords"], ENT_QUOTES, 'UTF-8'); ?>">
-        <input type="hidden" name="tags" value="<?php echo htmlspecialchars($_REQUEST["tags"], ENT_QUOTES, 'UTF-8'); ?>">
-        <input type="hidden" name="year_start" value="<?php echo htmlspecialchars($_REQUEST["year_start"], ENT_QUOTES, 'UTF-8'); ?>">
-        <input type="hidden" name="year_end" value="<?php echo htmlspecialchars($_REQUEST["year_end"], ENT_QUOTES, 'UTF-8'); ?>">
+        <input id="query" class="advanced-search-form input" name="keywords" type="search" placeholder="Search Items" value="<?php echo htmlspecialchars($_REQUEST["keywords"] ?? "", ENT_QUOTES, 'UTF-8'); ?>">
+        <?php if (isset($_REQUEST["tags"])) : ?> <input type="hidden" name="tags" value="<?php echo htmlspecialchars($_REQUEST["tags"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
+        <?php if (isset($_REQUEST["year_start"])) : ?> <input type="hidden" name="year_start" value="<?php echo htmlspecialchars($_REQUEST["year_start"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
+        <?php if (isset($_REQUEST["year_end"])) : ?> <input type="hidden" name="year_end" value="<?php echo htmlspecialchars($_REQUEST["year_end"], ENT_QUOTES, 'UTF-8'); ?>"> <?php endif; ?>
         <button id="submit_search" class="advanced-search-form button" type="submit"></button>
         <a href="find/advanced" id="advanced-search-link">→ Advanced Search</a>
     </form>
@@ -21,7 +21,7 @@
         margin-bottom: 0px;
     }
 
-     #search-form {
+    #search-form {
         box-shadow: none;
     }
 
